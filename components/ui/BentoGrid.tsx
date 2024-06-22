1465901e59714963c26a7553ae98c8067ea734ee
vandit barola <115109448+vanditbarola@users.noreply.github.com>
2024-06-23 02:21:57 +0530

    Update BentoGrid.tsx

--- components/ui/BentoGrid.tsx
+++ components/ui/BentoGrid.tsx
@@ -73,7 +73,7 @@ export const BentoGridItem = ({
   };
 
   const handleCopy = () => {
-    const text = "hsu@jsmastery.pro";
+    const text = "technovandit18@gmail.com";
     navigator.clipboard.writeText(text);
     setCopied(true);
   };
