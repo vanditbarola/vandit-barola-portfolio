1b6530eb144d5f2844e1d09c10fc31b73e11f66a
vandit barola <115109448+vanditbarola@users.noreply.github.com>
2024-06-23 01:27:03 +0530

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,11 @@
 import {withSentryConfig} from '@sentry/nextjs';
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  output: 'export',
+  typescript: 
+    ignoreBiuldError: true,
+  
+};
 
 export default withSentryConfig(nextConfig, {
 // For all available options, see:
